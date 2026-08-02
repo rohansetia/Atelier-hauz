b46251675d08eb14a419154fc27b74f4ce1140ab
rohansetia <setiarohit770@gmail.com>
2026-08-02 20:43:01 +0530

    Fix schedule visit form

--- app/api/inquiry/route.ts
+++ app/api/inquiry/route.ts
@@ -46,7 +46,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
     const emailResponse = await resend.emails.send({
       from: "AtelierHauz <onboarding@resend.dev>",
-      to: process.env.TO_EMAIL || "atelierhauzin@gmail.com",
+      to: process.env.TO_EMAIL || "atelierhausinterior@gmail.com",
       subject: `🏡 New Site Visit Request - ${name}`,
 
       html: `
