9948082d6467b8d347f3740f255247744b2efcc8
rohansetia <setiarohit770@gmail.com>
2026-08-02 20:33:00 +0530

    Fix logo and favicon

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center gap-8">
 
           <Image
-            src="/logo.jpeg"
+            src="/logo.svg"
             alt="Atelier Hauz"
             width={180}
             height={60}
